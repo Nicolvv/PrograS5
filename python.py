425c3bab77574667935b5809248c08e294ffa394
Nicolvv <nic.valero@duocuc.cl>
2025-06-26 10:26:04 -0400

    version restar

--- python.py
+++ python.py
@@ -1,6 +1,12 @@
-def sumar():
-    return n1 + n2
+def sumar(a,b):
+    return a + b
+
+def restar(a,b):
+    return a-b
+
 
 n1 = int(input("Ingresar numero: "))
 n2 = int(input("Ingresar numero: "))
 print(sumar(n1,n2))
+print(restar(n1,n2))
+
